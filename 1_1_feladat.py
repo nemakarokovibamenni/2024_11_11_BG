46de260c7205cf04e5d5debe0338555ef639160c
nemakarokovibamenni <barsony.gergely@diak.szbi-pg.hu>
2024-11-11 10:24:31 +0100

    add solution

--- 1_1_feladat.py
+++ 1_1_feladat.py
@@ -3,6 +3,13 @@
 A program a bekért neveket írja ki a képernyőre!Készíts egy programot, amely a felhasználótól keresztneveket kér be 
 egészen addig, amíg az ENTER-t nem üt (nem ad meg újabb nevet a bekérésnél)! A program a bekért neveket írja ki a képernyőre!'''
 
+
+nevek = []
 folytat = True
 while folytat:
-    nev = input('')
+    nev = input('Kérlek adj meg egy keresztnevet: ')
+    if nev == "":
+        break 
+    nevek.append(nev)
+
+print(nevek)
