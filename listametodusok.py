a0eb16eee9fb1b3d983d58be292442d40e537734
nemakarokovibamenni <barsony.gergely@diak.szbi-pg.hu>
2024-11-13 14:22:50 +0100

    Create listametodusok.py

---
+++ listametodusok.py
@@ -0,0 +1,24 @@
+nyelvek = ['Python', 'C', 'C++', 'Java']
+#változóhoz mentve megtartja az eredeti listát is
+nyelvek2 = sorted(nyelvek)
+print(nyelvek2)
+print(nyelvek)
+
+#sorbarendezi a listát
+nyelvek.sort()
+print(nyelvek)
+
+#fordított sorrendbe rendezi a listát
+nyelvek.reverse
+print(nyelvek)
+
+
+print(nyelvek.index('C'))
+
+print(nyelvek.count('Python'))
+
+print('C++' in nyelvek)
+print('C+++' in nyelvek)
+
+nev = "Hello"
+print(nev.index("l"))
